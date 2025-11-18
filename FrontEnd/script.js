b2ea6b7441047401ee241f284604ef8c47fad75a
MaxGOUET <maxime.gouet1@gmail.com>
2025-11-18 15:10:24 +0100

    add filters btn with JS fetch API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,6 @@
-async function getData() {
+// on recupere les données de l'API
+
+async function getDataWorks() {
   const url = "http://localhost:5678/api/works";
   try {
     const response = await fetch(url);
@@ -12,11 +14,27 @@ async function getData() {
     console.error(error.message);
   }
 }
+async function getDataCategories() {
+  const url = "http://localhost:5678/api/categories";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+// On récupére les élements du DOM et on affiche le resultats du retour de l'API pour image et titre.
 
 const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
 async function showResult() {
-  let results = await getData();
+  let results = await getDataWorks();
   function createGallery(results) {
     results.forEach((element) => {
       let figure = document.createElement("figure");
@@ -34,3 +52,20 @@ async function showResult() {
 }
 
 showResult();
+showFilters();
+
+// on creer les boutons de filtrages des elements
+async function showFilters() {
+  let results = await getDataCategories();
+  let btnFilterAll = document.createElement("button");
+  filters.appendChild(btnFilterAll);
+  btnFilterAll.innerText = "Tous";
+  function createFilters(results) {
+    results.forEach((element) => {
+      let btnFilters = document.createElement("button");
+      filters.appendChild(btnFilters);
+      btnFilters.innerText = element.name;
+    });
+  }
+  createFilters(results);
+}
