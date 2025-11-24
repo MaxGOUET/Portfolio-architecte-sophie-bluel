9404177af6b88b714768876b13b2c07f5aedf5ae
MaxGOUET <maxime.gouet1@gmail.com>
2025-11-24 12:40:03 +0100

    modifs form connect

---
+++ FrontEnd/login.js
@@ -0,0 +1,20 @@
+async function postLogin(donnees) {
+  const url = "http://localhost:5678/api/users/login";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(donnees),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
